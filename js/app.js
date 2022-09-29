f1ade7665a250ea3bf7022db81abfd834d487d01
Ameera Ali <amiraabdullamir@gmail.com>
2022-09-29 22:06:25 +0300

    Clicking bug fixed, reset added

--- js/app.js
+++ js/app.js
@@ -3,37 +3,42 @@ let symbols = ["X","O"];
 let gridItems = document.getElementsByClassName("grid-item");
 gridItems = [...gridItems]
 let turns = [[0],[0],[0],[0],[0],[0],[0],[0],[0]]
-let symbol1=""
-let symbol2=""
-let result =""
-let winner=""
-let isEmpty=""
+let symbol1 ,symbol2, winner, isEmpty=""
+let result =false;
+let symbolDec =0
 document.getElementById("status-header").innerText="It's "+status+"'s" + " turn"
 
 
-function clickCheck(){    
+
+function clickCheck(){  
+    removeE(event.target)  
     let updateNo="";
    let clickedElement ="";
-   clickedElement = event.target.id   
+   clickedElement = event.target.id 
 event.target.innerText= decideSymbol();
 updateNo = (status =="player-1")?updateNo=1: updateNo=2;
-   updates(updateNo, clickedElement);
-    result =checkWin();
+   updateArray(updateNo, clickedElement);
+    
+    result=checkWin()
     if(result==true){
+        document.getElementById("mess").innerText=winner + " has won"
     document.getElementById("blocker").style.visibility = "visible";
-    document.getElementById("blocker-mess").innerText= winner+" has won"
     document.getElementById("blocker-mess").style.visibility = "visible";
-
+    let reButton= document.getElementById("reButton");
+    reButton.addEventListener("click",reset)
 
     }else{
         isEmpty=checkFull(gridItems)
         if(isEmpty==true){
+            document.getElementById("mess").innerText=" It's a tie"
             document.getElementById("blocker").style.visibility = "visible";
-            document.getElementById("blocker-mess").innerText= "It's a tie"
             document.getElementById("blocker-mess").style.visibility = "visible";
+            let reButton= document.getElementById("reButton");
+            reButton.addEventListener("click",reset)
+
 
         }
-        event.target.removeEventListener("click", clickCheck) 
+        updateUI(updateNo);
 
 
 
@@ -42,36 +47,54 @@ updateNo = (status =="player-1")?updateNo=1: updateNo=2;
 }
 
 gridItems.forEach(block =>{
-    block.addEventListener("click", clickCheck)})
+    block.addEventListener("click", clickCheck)
+
+
+})
+
+function removeE(element){
+    element.removeEventListener("click",clickCheck)
+    console.log("removed")
+
+}
 
     
 //decide which symbol
 function decideSymbol(){  
-    let symbolDec = Math.floor(Math.random()*2)
-    if(status=="player-1" && turns.every(arr=>arr[0]==0)){  
+    //if it's first turn
+    if(status=="player-1" && turns.every(arr=>arr[0]==0)){ 
+        symbolDec = Math.floor(Math.random()*2)
         symbol1 =symbols[symbolDec];   
         if(symbol1=="X"){
         symbol2 ="O"
+     
+
         }else{
             symbol2="X"
-        }
+           
+        }        
+
         return symbol1
+
     }else{      
 
         if(status=="player-1"){
+
             return symbol1
 
         }
-        if(status=="player-2"){
+        if(status=="player-2"){        
+
             return symbol2
 
         }
         
     }
+
     
 }
-function updates(updateNo, eventEle){
-    //update array and status
+function updateUI(updateNo){
+    console.log("UI");
     (updateNo==1)?status="player-2": status="player-1"
     document.getElementById("status-header").innerText="It's "+status+"'s" + " turn"
     if(status=="player-1"){
@@ -79,9 +102,9 @@ function updates(updateNo, eventEle){
 
     }else{
         document.getElementById("status-header").style.background="pink"
-
     }
-
+}
+function updateArray(updateNo, eventEle){
     if(eventEle=="grid-1"){
     turns[0][0]=updateNo
     }else if(eventEle=="grid-2"){
@@ -110,71 +133,26 @@ function updates(updateNo, eventEle){
     }
 
 }
+
 function checkWin(){
-    //checks rows
-    let win = false
-for(let i=0;i<=3;i+=3){
-  if(turns[i][0]==turns[i+1][0]&&turns[i][0]==turns[i+2]){
-    if(turns[i][0]!=0 && turns[i+2][0]!=0 && turns[i+1][0]!=0){
-        if(turns[i][0]==1){
-            winner="player 1"
-            win=true
-            return win
-        }else{
-            winner="player 2"
-            win=true
-            return win
-        }
+    let win=false;
+    console.log("running win")
+ let winningIndex=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8]]
+for(let i=0;i<winningIndex.length;i++){
+    if(turns[winningIndex[i][0]][0]==turns[winningIndex[i][1]][0] && 
+        turns[winningIndex[i][0]][0]==turns[winningIndex[i][2]][0]){
+            if(turns[winningIndex[i][0]][0]!=0 && turns[winningIndex[i][1]][0]!=0 && 
+           turns[winningIndex[i][2]][0]!=0){
+            win=true;
+            winner=status
+            return win;
         
-       
-  }
-}
-if(turns[i][0]==turns[i+3][0]&&turns[i][0]==turns[i+6]){
-    if(turns[i][0]!=0 && turns[i+3][0]!=0 && turns[i+6][0]!=0){
-        if(turns[i][0]==1){
-            winner="player 1"
-            win=true
-            return win
-        }else{
-            winner="player 2"
-            win=true
-            return win
-        }
         
-  }
-}
-if(turns[i][0]==2|| turns[i][0]==0){
-if(turns[i][0]==turns[i+4][0]&& turns[i][0]==turns[i+8]){
-    if(turns[i][0]!=0 && turns[i+4][0]!=0 && turns[i+8][0]!=0){
-        if(turns[i][0]==1){
-            winner="player 1"
-            win=true
-            return win
-        }else{
-            winner="player 2"
-            win=true
-            return win
-        }
-        
-  }
-}
-if(turns[i][0]==turns[i+2][0]&& turns[i][0]==turns[i+4]){
-    if(turns[i][0]!=0 && turns[i+2][0]!=0 && turns[i+4][0]!=0){
-        if(turns[i][0]==1){
-            winner="player 1"
-            win=true
-            return win
-        }else{
-            winner="player 2"
-            win=true
-            return win
-        }
-        
-  }
+    }
 }
 }
 }
-}
+
 
 function checkFull(arr){
    let fullCheck;
@@ -185,6 +163,23 @@ function checkFull(arr){
  }
 }          
 
+function reset(){
+status ="player-1";
+gridItems.forEach(item=>item.innerText="")
+ turns = [[0],[0],[0],[0],[0],[0],[0],[0],[0]]
+ symbol1 ,symbol2, winner, isEmpty=""
+ result =false;
+ symbolDec =0
+ document.getElementById("mess").innerText=" "
+ document.getElementById("blocker").style.visibility = "hidden";
+ document.getElementById("blocker-mess").style.visibility = "hidden";
+ document.getElementById("status-header").innerText="It's "+status+"'s" + " turn"
+ gridItems.forEach(block =>{
+    block.addEventListener("click", clickCheck)
 
-        
+
+})
+
+
+}
     
